a00023fedf636cd1892654b9d7bdfd7874f46968
deepakgudi-pixel <75731075+deepakgudi-pixel@users.noreply.github.com>
2021-03-27 18:56:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const date = require(__dirname + "/date.js")
+
+const app = express()
+
+const items = [];
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(express.static("public"))
+
+app.get("/",(req, res)=>{
+   
+   const day = date.getDate()
+    
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    })
+
+
+})
+
+app.post("/", (req, res)=>{
+const item = req.body.newItem
+items.push(item)
+res.redirect("/")
+})
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>console.log(`server running on ${PORT}`))
